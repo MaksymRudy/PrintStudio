d4cc8234ed1b8989b3337991c32f498aa8eafe07
MaksymRudy <mail@google.com>
2023-05-10 19:55:52 +0300

    create interface CrudOp and implement

---
+++ src/service/CrudOperations.java
@@ -0,0 +1,19 @@
+package service;
+
+public interface CrudOperations {
+    static void create() {
+        System.out.println("Create");
+    }
+
+    static void read() {
+        System.out.println("Read");
+    }
+
+    static void update() {
+        System.out.println("Update");
+    }
+
+    static void delete() {
+        System.out.println("Delete");
+    }
+}
