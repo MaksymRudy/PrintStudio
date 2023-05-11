7ebd7fb3da5650bd7a3ae58a72a6224c90e6e197
MaksymRudy <mail@google.com>
2023-05-11 13:20:50 +0300

    create exit from menu

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,12 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String menu = menu();
         System.out.println(menu);
-
-        while (true) {
+        int menuNumber = 0;
+        while (menuNumber >= 0) {
             // Reading data using readLine
+            System.out.println("Enter number");
             String number = reader.readLine();
-            int menuNumber = Integer.parseInt(number);
+            menuNumber = Integer.parseInt(number);
 
             // use switch for choosing method
             switch (menuNumber) {
@@ -23,18 +24,20 @@ public class Main {
                 case 2 -> companyUserCRUD();
                 case 3 -> priceCRUD();
                 case 4 -> productCRUD();
+                case -1 -> System.out.println("we exit from menu");
                 default -> System.out.println("wrong number");
             }
         }
-
     }
+
 
     private static String menu() {
         return "Menu (Choose number):" + "\n" +
                 "1. Create an order" + "\n" +
                 "2. Company/User CRUD" + "\n" +
                 "3. Price CRUD" + "\n" +
-                "4. Product CRUD";
+                "4. Product CRUD" + "\n" +
+                "-1. Exit";
     }
 
     private static void createAnOrder() {
